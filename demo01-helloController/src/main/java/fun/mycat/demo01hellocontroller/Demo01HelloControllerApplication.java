5f8a3c7c8fcde723c78d26e58efe0ad92a4f65f0
plotmax <minevitably@gmail.com>
2024-06-21 12:24:19 +0800

    v1.01 使用Controller

---
+++ demo01-helloController/src/main/java/fun/mycat/demo01hellocontroller/Demo01HelloControllerApplication.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo01hellocontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01HelloControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01HelloControllerApplication.class, args);
+    }
+
+}
---
+++ demo01-helloController/src/test/java/fun/mycat/demo01hellocontroller/Demo01HelloControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo01hellocontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01HelloControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
