feae76470c7b367aa5eff6d7ac3b2977a25f68a6
plotmax <minevitably@gmail.com>
2024-06-21 12:40:03 +0800

    v1.04 使用lombok提供的日志工具slf4j

---
+++ demo04-slf4j/src/main/java/fun/mycat/demo04slf4j/Demo04Slf4jApplication.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo04slf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo04Slf4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo04Slf4jApplication.class, args);
+    }
+
+}
---
+++ demo04-slf4j/src/test/java/fun/mycat/demo04slf4j/Demo04Slf4jApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo04slf4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo04Slf4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
