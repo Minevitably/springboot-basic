1d0213c259834167c0b844119ca1a6b6b0de3a00
plotmax <minevitably@gmail.com>
2024-06-21 12:29:40 +0800

    v1.03 使用应用健康监控工具

---
+++ demo03-actuator/src/main/java/fun/mycat/demo03actuator/Demo03ActuatorApplication.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo03actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo03ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo03ActuatorApplication.class, args);
+    }
+
+}
---
+++ demo03-actuator/src/test/java/fun/mycat/demo03actuator/Demo03ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo03actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo03ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
