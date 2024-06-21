c4c825b76d952fa92ce9586b410d7cdedc3d53c0
plotmax <minevitably@gmail.com>
2024-06-21 12:20:04 +0800

    v1.00 使用阿里云初始化springboot项目

---
+++ demo00-hello/src/main/java/fun/mycat/demo00hello/Demo00HelloApplication.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo00hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo00HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo00HelloApplication.class, args);
+    }
+
+}
---
+++ demo00-hello/src/test/java/fun/mycat/demo00hello/Demo00HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mycat.demo00hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo00HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
